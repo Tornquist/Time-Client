4617c05e36c1ed05a7875b8e41912be522ff1249
Nathan Tornquist <Tornquist@live.com>
2018-12-12 08:20:16 -0600

    Move Shared to a single framework and test build system for macOS and iOS

--- Shared/Config/Time_Shared.h
+++ Shared/Config/Time_Shared.h
@@ -1,6 +1,6 @@
 //
-//  Time_Shared_macOS.h
-//  Time-Shared-macOS
+//  Time_Shared.h
+//  Time-Shared
 //
 //  Created by Nathan Tornquist on 12/9/18.
 //  Copyright © 2018 nathantornquist. All rights reserved.
@@ -8,11 +8,11 @@
 
 #import <Cocoa/Cocoa.h>
 
-//! Project version number for Time_Shared_macOS.
-FOUNDATION_EXPORT double Time_Shared_macOSVersionNumber;
+//! Project version number for Time_Shared.
+FOUNDATION_EXPORT double Time_SharedVersionNumber;
 
-//! Project version string for Time_Shared_macOS.
-FOUNDATION_EXPORT const unsigned char Time_Shared_macOSVersionString[];
+//! Project version string for Time_Shared.
+FOUNDATION_EXPORT const unsigned char Time_SharedVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Time_Shared_macOS/PublicHeader.h>
 
