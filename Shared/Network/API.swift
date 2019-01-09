87022144606b9e6d3bfa2f6e10d5e3de448e629c
Nathan Tornquist <Tornquist@live.com>
2019-01-09 07:31:58 -0600

    Add token refresh and tests

--- Shared/Network/API.swift
+++ Shared/Network/API.swift
@@ -70,4 +70,54 @@ class API {
         
         task.resume()
     }
+    
+    func refreshToken(with token: Token, completionHandler: @escaping (Token?, Error?) -> ()) {
+        guard var path = URL(string: baseURL) else {
+            completionHandler(nil, TimeError.unableToSendRequest("Cannot build URL"))
+            return
+        }
+        
+        path.appendPathComponent("/oauth/token")
+        var request = URLRequest(url: path)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        let rfc3986Reserved = CharacterSet(charactersIn: " %!*'();:@+$,/?#[]&=")
+        guard
+            let encodedToken: String = token.refresh.addingPercentEncoding(withAllowedCharacters: rfc3986Reserved.inverted)
+            else {
+                completionHandler(nil, TimeError.unableToSendRequest("Cannot encode credentials"))
+                return
+        }
+        
+        let bodyString = "grant_type=refresh_token&refresh_token=\(encodedToken)"
+        guard let bodyData = bodyString.data(using: String.Encoding.utf8) else {
+            completionHandler(nil, TimeError.unableToSendRequest("Cannot encode credentials"))
+            return
+        }
+        
+        request.httpBody = bodyData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                let message = error as? String ?? ""
+                completionHandler(nil, TimeError.requestFailed(message))
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                completionHandler(nil, TimeError.httpFailure(httpStatus.statusCode.description))
+                return
+            }
+            
+            do {
+                let token = try JSONDecoder().decode(Token.self, from: data)
+                completionHandler(token, nil)
+            } catch {
+                completionHandler(nil, TimeError.unableToDecodeResponse())
+            }
+        }
+        
+        task.resume()
+    }
 }
---
+++ Shared/Tests/Test_API.swift
@@ -0,0 +1,99 @@
+//
+//  Test_API.swift
+//  Shared-Tests
+//
+//  Created by Nathan Tornquist on 1/9/19.
+//  Copyright © 2019 nathantornquist. All rights reserved.
+//
+
+import Foundation
+
+import XCTest
+@testable import Time
+
+class Test_API: XCTestCase {
+    
+    override func setUp() { }
+    
+    override func tearDown() { }
+    
+    func test_getToken_validCredentials() {
+        let expectation = self.expectation(description: "getToken")
+        
+        API.shared.getToken(with: "test@test.com", and: "defaultPassword") { (token, error) in
+            XCTAssertNotNil(token)
+            XCTAssertNil(error)
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func test_getToken_invalidCredentials() {
+        let expectation = self.expectation(description: "getToken")
+        
+        API.shared.getToken(with: "test@test.com", and: "madeUpPassword") { (token, error) in
+            XCTAssertNil(token)
+            XCTAssertNotNil(error)
+            
+            switch error! {
+            case TimeError.httpFailure("401"):
+                XCTAssertTrue(true)
+            default:
+                XCTAssertFalse(true, "Unexpected error returned")
+                break
+            }
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func test_refreshToken_validRefresh() {
+        let expectation = self.expectation(description: "refreshToken")
+        
+        API.shared.getToken(with: "test@test.com", and: "defaultPassword") { (token, error) in
+            XCTAssertNotNil(token)
+            
+            API.shared.refreshToken(with: token!) { (token, error) in
+                XCTAssertNotNil(token)
+                XCTAssertNil(error)
+                
+                expectation.fulfill()
+            }
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func test_refreshToken_invalidRefresh() {
+        let expectation = self.expectation(description: "refreshToken")
+        
+        let token = Token(
+            userID: 1,
+            creation: Date(),
+            expiration: Date(),
+            token: "abcd",
+            refresh: "notReal"
+        )
+        
+        API.shared.refreshToken(with: token) { (token, error) in
+            XCTAssertNil(token)
+            XCTAssertNotNil(error)
+            
+            switch error! {
+            case TimeError.httpFailure("400"): // Made up token not found
+                XCTAssertTrue(true)
+            default:
+                XCTAssertFalse(true, "Unexpected error returned")
+                break
+            }
+            
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+}
