4d27bd79737da106bc94288d8f4d64257806225a
Nathan Tornquist <Tornquist@live.com>
2019-01-22 08:12:51 -0600

    Functional TokenStore

--- Shared/Helpers/TokenStore.swift
+++ Shared/Helpers/TokenStore.swift
@@ -10,19 +10,28 @@ import Foundation
 
 class TokenStore {
     static let prefix = "com.nathantornquist.Time.keys"
-    static let tag = prefix + ".token"
+    static let tag = (prefix + ".token")
     
     static func getToken() -> Token? {
         let getQuery: [String: Any] = [
-            kSecClass as String: kSecClassKey,
-            kSecAttrApplicationTag as String: TokenStore.tag,
-            kSecReturnRef as String: true
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: TokenStore.tag,
+            kSecReturnData as String: true,
+            kSecMatchLimit as String: kSecMatchLimitOne
         ]
         
         var item: AnyObject?
         let status = SecItemCopyMatching(getQuery as CFDictionary, &item)
+        guard status != errSecItemNotFound else {
+            print("Not found")
+            return nil
+        }
         guard status == errSecSuccess else {
-            print("SecItemCopyMatchingStatus: \(status)")
+            if let err = SecCopyErrorMessageString(status, nil) {
+                print("Read failed: \(err)")
+            } else {
+                print("SecItemCopyMatchingStatus: \(status)")
+            }
             return nil
         }
         guard let data = item as? Data else {
@@ -34,19 +43,31 @@ class TokenStore {
     }
     
     static func storeToken(_ token: Token) -> Bool {
-        guard let tokenData = try? JSONEncoder().encode(token) else {
+        guard let tokenData = try? JSONEncoder().encode(token),
+            let tokenString = String(data: tokenData, encoding: String.Encoding.utf8) else {
             return false
         }
+        let deleteQuery: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: TokenStore.tag
+        ]
         let addQuery: [String: Any] = [
-            kSecClass as String: kSecClassKey,
-            kSecAttrApplicationTag as String: TokenStore.tag,
-            kSecValueData as String: tokenData
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: TokenStore.tag,
+            kSecValueData as String: tokenString
         ]
         
-        let status = SecItemAdd(addQuery as CFDictionary, nil)
+        let statusDelete = SecItemDelete(deleteQuery as CFDictionary)
+        if (statusDelete != errSecSuccess) {    // Always check the status
+            if let err = SecCopyErrorMessageString(statusDelete, nil) {
+                print("Delete failed: \(err)")
+            }
+        }
         
-        if (status != errSecSuccess) {    // Always check the status
-            if let err = SecCopyErrorMessageString(status, nil) {
+        let statusAdd = SecItemAdd(addQuery as CFDictionary, nil)
+        
+        if (statusAdd != errSecSuccess) {    // Always check the status
+            if let err = SecCopyErrorMessageString(statusAdd, nil) {
                 print("Write failed: \(err)")
             }
             return false
