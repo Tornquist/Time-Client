1b7f7dc591c2b791935d3f70a0b1ce9c6b0ee22b
Nathan Tornquist <Tornquist@live.com>
2019-03-11 21:06:56 -0500

    Auto (or manually) login from main macOS window -- needs updated view management

---
+++ macOS/macOS/LoginViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Time-macOS
+//
+//  Created by Nathan Tornquist on 12/9/18.
+//  Copyright © 2018 nathantornquist. All rights reserved.
+//
+
+import Cocoa
+import TimeSDK
+
+class LoginViewController: NSViewController {
+
+    @IBOutlet weak var usernameTextField: NSTextField!
+    @IBOutlet weak var passwordTextField: NSSecureTextField!
+    @IBOutlet weak var loginButton: NSButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        
+        Time.shared.initialize { error in
+            if error == nil { self.openApplication() }
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func loginTapped(_ sender: NSButton) {
+        loginUser()
+    }
+    
+    // MARK: - Data Management
+    
+    func loginUser() {
+        let username = self.usernameTextField.stringValue
+        let password = self.passwordTextField.stringValue
+        Time.shared.authenticate(username: username, password: password) { error in
+            if error == nil {
+                self.openApplication()
+            }
+        }
+    }
+    
+    func openApplication() {
+        let action = {
+            self.performSegue(withIdentifier: "login", sender: nil)
+        }
+        
+        let isMain = Thread.main.isMainThread
+        if isMain { action() }
+        else { DispatchQueue.main.async { action() } }
+    }
+}
