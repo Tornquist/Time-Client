7ec8fb4b93e5000b937fb9cb250d8d25033cba65
Nathan Tornquist <Tornquist@live.com>
2019-04-15 07:03:17 -0500

    Add navigation control to homeVC with tableView

--- iOS/iOS/HomeViewController.swift
+++ iOS/iOS/HomeViewController.swift
@@ -9,18 +9,42 @@
 import UIKit
 import TimeSDK
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var signOutButton: UIButton!
+    var signOutButton: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.signOutButton.setTitle(NSLocalizedString("Sign Out", comment: ""), for: .normal)
+        self.configureTheme()
+    }
+    
+    func configureTheme() {
+        self.signOutButton = UIBarButtonItem(title: NSLocalizedString("Sign Out", comment: ""), style: .plain, target: self, action: #selector(signOutPressed(_:)))
+        self.navigationItem.leftBarButtonItem = self.signOutButton
+        
+        let topConstraint = NSLayoutConstraint(item: self.view, attribute: .top, relatedBy: .equal, toItem: self.tableView, attribute: .top, multiplier: 1, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: self.view, attribute: .bottom, relatedBy: .equal, toItem: self.tableView, attribute: .bottom, multiplier: 1, constant: 0)
+        self.view.addConstraints([topConstraint, bottomConstraint])
     }
     
     @IBAction func signOutPressed(_ sender: Any) {
         Time.shared.deauthenticate()
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - Table View
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath)
+        return cell
     }
 }
