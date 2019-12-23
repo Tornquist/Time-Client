ac21eb3fdf1544dc5d4f52152dbd935b76221334
Nathan Tornquist <Tornquist@live.com>
2019-12-23 09:32:20 -0600

    Allow unix or date/time columns to be specified for input files

--- iOS/iOS/NewImportViewController.swift
+++ iOS/iOS/NewImportViewController.swift
@@ -29,13 +29,44 @@ class NewImportViewController: UIViewController, UIDocumentPickerDelegate, UIPic
     @IBOutlet weak var step3Label: UILabel!
     @IBOutlet weak var step3StatusLabel: UILabel!
     var step3CategoryColumns: [String] = []
-    var step3AvailableCategoryColumns: [String] = []
     @IBOutlet weak var step3AddButton: UIButton!
     @IBOutlet weak var step3ContinueButton: UIButton!
     
+    @IBOutlet weak var step4Container: UIView!
+    @IBOutlet weak var step4Label: UILabel!
+    @IBOutlet weak var step4SegmentedControl: UISegmentedControl!
+    @IBOutlet weak var step4ColumnALabel: UILabel!
+    @IBOutlet weak var step4ColumnAButton: UIButton!
+    @IBOutlet weak var step4ColumnBLabel: UILabel!
+    @IBOutlet weak var step4ColumnBButton: UIButton!
+    @IBOutlet weak var step4ColumnCContainer: UIStackView!
+    @IBOutlet weak var step4ColumnCLabel: UILabel!
+    @IBOutlet weak var step4ColumnCButton: UIButton!
+    @IBOutlet weak var step4DateContainer: UIStackView!
+    @IBOutlet weak var step4DateFormatLabel: UILabel!
+    @IBOutlet weak var step4DateFormatTextField: UITextField!
+    @IBOutlet weak var step4TimeContainer: UIStackView!
+    @IBOutlet weak var step4TimeFormatLabel: UILabel!
+    @IBOutlet weak var step4TimeFormatTextField: UITextField!
+    @IBOutlet weak var step4TimezoneLabel: UILabel!
+    @IBOutlet weak var step4TimezoneTextField: UITextField!
+    @IBOutlet weak var step4TestButton: UIButton!
+    @IBOutlet weak var step4TestLabel: UILabel!
+    @IBOutlet weak var step4ApproveButton: UIButton!
+    
+    var step4ColumnAName: String? = nil
+    var step4ColumnBName: String? = nil
+    var step4ColumnCName: String? = nil
+    
+    // Shared
+    
+    var availableColumns: [String] = []
+    
     enum PickerViewMode {
         case categoryColumns
-        case dateColumns
+        case columnA
+        case columnB
+        case columnC
     }
     var pickerViewMode: PickerViewMode = .categoryColumns
     var pickerViewSelectedValue: String? = nil
@@ -66,6 +97,65 @@ class NewImportViewController: UIViewController, UIDocumentPickerDelegate, UIPic
         self.dismiss(animated: true, completion: nil)
     }
     
+    func generateAvailableColumns() {
+        guard let loadedColumns = self.importer?.columns else {
+            self.availableColumns = []
+            return
+        }
+        self.availableColumns = loadedColumns.filter { (column) -> Bool in
+            return !self.step3CategoryColumns.contains(column)
+        }
+    }
+    
+    @IBAction func showColumnPicker(_ sender: UIButton) {
+        self.generateAvailableColumns()
+        
+        guard self.availableColumns.count > 0 else {
+            let title = NSLocalizedString("No columns remain", comment: "")
+            let vc = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+            vc.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
+            self.present(vc, animated: true, completion: nil)
+            return
+        }
+        
+        let title = NSLocalizedString("Select column", comment: "")
+        let vc = UIAlertController(title: title, message: "\n\n\n\n\n\n", preferredStyle: .alert)
+        
+        let pickerView = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        vc.view.addSubview(pickerView)
+        
+        self.pickerViewSelectedValue = self.availableColumns[0]
+        if sender == self.step3AddButton {
+            self.pickerViewMode = .categoryColumns
+        } else if (sender == self.step4ColumnAButton) {
+            self.pickerViewMode = .columnA
+        } else if (sender == self.step4ColumnBButton) {
+            self.pickerViewMode = .columnB
+        } else if (sender == self.step4ColumnCButton) {
+            self.pickerViewMode = .columnC
+        } else {
+            return // Should never occur unless sender names are mismatched
+        }
+
+        vc.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil))
+        vc.addAction(UIAlertAction(title: NSLocalizedString("Confirm", comment: ""), style: .default, handler: { action in
+            if self.pickerViewSelectedValue != nil {
+                if self.pickerViewMode == .categoryColumns {
+                    self.step3ColumnSelected()
+                } else {
+                    self.step4ColumnSelected()
+                }
+            }
+            
+            self.pickerViewSelectedValue = nil
+        }))
+
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    
     func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
         // No action
     }
@@ -82,7 +172,10 @@ class NewImportViewController: UIViewController, UIDocumentPickerDelegate, UIPic
         self.step3Label.text = NSLocalizedString("3. Set columns for tree creation.\n*First is parent, second is child, third is grandchild, etc.", comment: "")
         self.step3AddButton.setTitle(NSLocalizedString("Add column", comment: ""), for: .normal)
         self.step3ContinueButton.setTitle(NSLocalizedString("Continue", comment: ""), for: .normal)
-        self.step3ContinueButton.isHidden = true
+        self.step3ContinueButton.isEnabled = false
+        
+        self.step4Label.text = NSLocalizedString("4. Specify date formatting rules", comment: "")
+        self.configureStep4()
         
         self.restartSteps()
     }
@@ -100,6 +193,8 @@ class NewImportViewController: UIViewController, UIDocumentPickerDelegate, UIPic
         self.step3Container.isHidden = true
         self.step3StatusLabel.text = ""
         self.step3CategoryColumns = []
+        
+        self.step4Container.isHidden = true
     }
     
     // MARK: Step 1
@@ -162,54 +257,169 @@ class NewImportViewController: UIViewController, UIDocumentPickerDelegate, UIPic
     
     // MARK: Step 3
     
-    func generateAvailableCategoryColumns() {
-        guard let loadedColumns = self.importer?.columns else {
-            self.step3AvailableCategoryColumns = []
-            return
-        }
-        self.step3AvailableCategoryColumns = loadedColumns.filter { (column) -> Bool in
-            return !self.step3CategoryColumns.contains(column)
-        }
+    func step3ColumnSelected() {
+        self.step3CategoryColumns.append(self.pickerViewSelectedValue!)
+        
+        self.step3StatusLabel.text = self.step3CategoryColumns.joined(separator: "\n")
+        self.step3ContinueButton.isEnabled = true
+    }
+
+    @IBAction func step3ContinueButtonPressed(_ sender: Any) {
+        self.importer?.categoryColumns = self.step3CategoryColumns
+        
+        self.step3Container.isHidden = true
+        self.step4Container.isHidden = false
     }
     
-    @IBAction func step3AddButtonPressed(_ sender: Any) {
-        self.generateAvailableCategoryColumns()
-        guard self.step3AvailableCategoryColumns.count > 0 else {
-            let title = NSLocalizedString("No columns remain", comment: "")
-            let vc = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-            vc.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
-            self.present(vc, animated: true, completion: nil)
-            return
+    // MARK: Step 4
+    
+    func configureStep4() {
+        self.step4SegmentedControl.removeAllSegments()
+        self.step4SegmentedControl.insertSegment(withTitle: NSLocalizedString("Date/Time", comment: ""), at: 0, animated: false)
+        self.step4SegmentedControl.insertSegment(withTitle: NSLocalizedString("Unix", comment: ""), at: 1, animated: false)
+        self.step4SegmentedControl.selectedSegmentIndex = 0
+        
+        self.step4TestButton.setTitle(NSLocalizedString("Test Formatting", comment: ""), for: .normal)
+            
+        self.step4ApproveButton.setTitle(NSLocalizedString("Continue", comment: ""), for: .normal)
+        
+        self.updateStep4Fields()
+    }
+    
+    func updateStep4Fields() {
+        // Reset. Only called 1st time, and on segment change
+        self.step4ColumnAName = nil
+        self.step4ColumnBName = nil
+        self.step4ColumnCName = nil
+        self.generateAvailableColumns()
+        
+        self.step4TestLabel.text = NSLocalizedString("<Format Test Output>", comment: "")
+        self.step4TestButton.isEnabled = false
+        self.step4ApproveButton.isEnabled = false
+        
+        if self.step4SegmentedControl.selectedSegmentIndex == 0 {
+            self.step4ColumnALabel.text = NSLocalizedString("Date Column", comment: "")
+            self.step4ColumnAButton.setTitle(NSLocalizedString("Select Column", comment: ""), for: .normal)
+            self.step4ColumnAButton.isEnabled = true
+            
+            self.step4ColumnBLabel.text = NSLocalizedString("Start Time Column", comment: "")
+            self.step4ColumnBButton.setTitle(NSLocalizedString("Select Column", comment: ""), for: .normal)
+            self.step4ColumnBButton.isEnabled = true
+            
+            self.step4ColumnCContainer.isHidden = false
+            self.step4ColumnCLabel.text = NSLocalizedString("End Time Column", comment: "")
+            self.step4ColumnCButton.setTitle(NSLocalizedString("Select Column", comment: ""), for: .normal)
+            self.step4ColumnCButton.isEnabled = true
+            
+            self.step4DateContainer.isHidden = false
+            self.step4DateFormatLabel.text = NSLocalizedString("Date Format", comment: "")
+            self.step4DateFormatTextField.text = "M/d/yy"
+            
+            self.step4TimeContainer.isHidden = false
+            self.step4TimeFormatLabel.text = NSLocalizedString("Time Format", comment: "")
+            self.step4TimeFormatTextField.text = "h:mm a"
+        } else {
+            self.step4ColumnALabel.text = NSLocalizedString("Start Timestamp", comment: "")
+            self.step4ColumnAButton.setTitle(NSLocalizedString("Select Column", comment: ""), for: .normal)
+            self.step4ColumnAButton.isEnabled = true
+            
+            self.step4ColumnBLabel.text = NSLocalizedString("End Timestamp", comment: "")
+            self.step4ColumnBButton.setTitle(NSLocalizedString("Select Column", comment: ""), for: .normal)
+            self.step4ColumnBButton.isEnabled = true
+            
+            self.step4ColumnCContainer.isHidden = true
+            self.step4DateContainer.isHidden = true
+            self.step4TimeContainer.isHidden = true
         }
         
-        let title = NSLocalizedString("Select column", comment: "")
-        let vc = UIAlertController(title: title, message: "\n\n\n\n\n\n", preferredStyle: .alert)
+        self.step4TimezoneLabel.text = NSLocalizedString("Timezone", comment: "")
+        self.step4TimezoneTextField.text = TimeZone.current.abbreviation() ?? "CST"
+    }
+    
+    @IBAction func step4DidToggleSegmentedControl(_ sender: Any) {
+        self.updateStep4Fields()
+    }
+    
+    func step4ColumnSelected() {
+        guard self.pickerViewSelectedValue != nil else { return }
         
-        let pickerView = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
-        pickerView.delegate = self
-        pickerView.dataSource = self
-        vc.view.addSubview(pickerView)
+        if self.pickerViewMode == .columnA {
+            self.step4ColumnAName = self.pickerViewSelectedValue
+            self.step4ColumnAButton.setTitle(self.pickerViewSelectedValue!, for: .normal)
+            self.step4ColumnAButton.isEnabled = false
+        } else if self.pickerViewMode == .columnB {
+            self.step4ColumnBName = self.pickerViewSelectedValue
+            self.step4ColumnBButton.setTitle(self.pickerViewSelectedValue!, for: .normal)
+            self.step4ColumnBButton.isEnabled = false
+        } else if self.pickerViewMode == .columnC {
+            self.step4ColumnCName = self.pickerViewSelectedValue
+            self.step4ColumnCButton.setTitle(self.pickerViewSelectedValue!, for: .normal)
+            self.step4ColumnCButton.isEnabled = false
+        }
         
-        self.pickerViewSelectedValue = self.step3AvailableCategoryColumns[0]
-        self.pickerViewMode = .categoryColumns
-
-        vc.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil))
-        vc.addAction(UIAlertAction(title: NSLocalizedString("Confirm", comment: ""), style: .default, handler: { action in
-            if self.pickerViewSelectedValue != nil {
-                self.step3CategoryColumns.append(self.pickerViewSelectedValue!)
-                
-                self.step3StatusLabel.text = self.step3CategoryColumns.joined(separator: "\n")
-                self.step3ContinueButton.isHidden = false
+        let aBSet = self.step4ColumnAName != nil && self.step4ColumnBName != nil
+        let cSet = self.step4ColumnCName != nil
+        let allSet = self.step4SegmentedControl.selectedSegmentIndex == 0 ? (aBSet && cSet) : aBSet
+        
+        self.step4TestButton.isEnabled = allSet
+    }
+    
+    @IBAction func step4TestFormatPressed(_ sender: Any) {
+        if self.step4SegmentedControl.selectedSegmentIndex == 0 {
+            guard
+                let dateColumn = self.step4ColumnAName,
+                let startTimeColumn = self.step4ColumnBName,
+                let endTimeColumn = self.step4ColumnCName,
+                let dateFormat = self.step4DateFormatTextField.text,
+                let timeFormat = self.step4TimeFormatTextField.text,
+                let timezone = self.step4TimezoneTextField.text else {
+                    // Show error
+                    return
             }
             
-            self.pickerViewSelectedValue = nil
-        }))
-
-        self.present(vc, animated: true, completion: nil)
-    }
-    
-    @IBAction func step3ContinueButtonPressed(_ sender: Any) {
-        self.importer?.categoryColumns = self.step3AvailableCategoryColumns
+            do {
+                let result = try self.importer?.setDateTimeParseRules(
+                    dateColumn: dateColumn,
+                    startTimeColumn: startTimeColumn,
+                    endTimeColumn: endTimeColumn,
+                    dateFormat: dateFormat,
+                    timeFormat: timeFormat,
+                    timezoneAbbreviation: timezone,
+                    testFormat: "MMM d, y @ h:mm a zzz"
+                )
+                let resultString = "First row raw input: \(result?.startRaw ?? "??")\nFirst row start parsed: \(result?.startParsed ?? "??")"
+                self.step4TestLabel.text = resultString
+                self.step4ApproveButton.isEnabled = true
+            } catch {
+                // Show error
+                self.step4TestLabel.text = NSLocalizedString("Unable to parse", comment: "")
+                self.step4ApproveButton.isEnabled = false
+            }
+        } else {
+            guard
+                let startUnix = self.step4ColumnAName,
+                let endUnix = self.step4ColumnBName,
+                let timezone = self.step4TimezoneTextField.text else {
+                    // Show error
+                    return
+            }
+            
+            do {
+                let result = try self.importer?.setDateTimeParseRules(
+                    startUnixColumn: startUnix,
+                    endUnixColumn: endUnix,
+                    timezoneAbbreviation: timezone,
+                    testFormat: "MMM d, y @ h:mm a zzz"
+                )
+                let resultString = "First row raw input: \(result?.startRaw ?? "??")\nFirst row start parsed: \(result?.startParsed ?? "??")"
+                self.step4TestLabel.text = resultString
+                self.step4ApproveButton.isEnabled = true
+            } catch {
+                // Show error
+                self.step4TestLabel.text = NSLocalizedString("Unable to parse", comment: "")
+                self.step4ApproveButton.isEnabled = false
+            }
+        }
     }
     
     // MARK: - UIDocumentPickerDelegate
@@ -233,31 +443,16 @@ class NewImportViewController: UIViewController, UIDocumentPickerDelegate, UIPic
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch self.pickerViewMode {
-        case .categoryColumns:
-            return self.step3AvailableCategoryColumns.count
-        case .dateColumns:
-            return 0
-        }
+        return self.availableColumns.count
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        switch self.pickerViewMode {
-        case .categoryColumns:
-            return self.step3AvailableCategoryColumns[row]
-        case .dateColumns:
-            return nil
-        }
+        return self.availableColumns[row]
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        switch self.pickerViewMode {
-        case .categoryColumns:
-            if self.step3AvailableCategoryColumns.count > 0 {
-                self.pickerViewSelectedValue = self.step3AvailableCategoryColumns[row]
-            }
-        case .dateColumns:
-            break
+        if self.availableColumns.count > 0 {
+            self.pickerViewSelectedValue = self.availableColumns[row]
         }
     }
 }
