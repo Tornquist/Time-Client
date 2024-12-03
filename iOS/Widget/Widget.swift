42aa85c4ba8c17434ef060ef11c5ee489a719c4e
Nathan Tornquist <Tornquist@live.com>
2024-12-02 22:09:49 -0600

    Widget containerBackground API

--- iOS/Widget/Widget.swift
+++ iOS/Widget/Widget.swift
@@ -271,8 +271,8 @@ struct TimeWidgetView : View {
             getBlock(forToday: false, andEntry: entry)
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .leading)
-        .padding()
         .background(Theme.background)
+        .containerBackground(Theme.background, for: .widget)
     }
     
     func getBlock(forToday: Bool, andEntry value: TimeEntry) -> some View {
